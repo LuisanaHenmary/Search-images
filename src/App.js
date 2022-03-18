123f2c2cd9bf381cdd87b046598d21765d1c6000
Luisana Perez <luisanahenmaryp@gmail.com>
2022-03-18 14:59:48 -0400

    create and add pictore component
    prototype call api with axios

--- src/App.js
+++ src/App.js
@@ -1,15 +1,43 @@
-import Header from "./components/Header";
-import Footer from "./components/Footer";
-import Content from "./components/Content";
+import { useCallback, useEffect, useState } from "react"
+import axios from "axios"
+import Header from "./components/Header"
+import Footer from "./components/Footer"
+import Content from "./components/Content"
+import Picture from "./components/Picture"
 
 const App = () => {
+
+  const [photos, setSPhotos] = useState([])
+  const apiKey = process.env.REACT_APP_API_KEY
+  const search = "cat"
+
+  const cargaSimbolos = useCallback(async () => {
+    try {
+      const responce = await axios.get(
+        `https://api.unsplash.com/search/photos?per_page=20&query=${search}█`,
+        {
+          headers: {
+            'Authorization': `Client-ID ${apiKey}`
+          }
+        })
+        setSPhotos(responce.data.results)
+    } catch (error) {
+      console.log('error')
+    }
+  }, [search, apiKey])
+
+
+  useEffect(() => {
+    cargaSimbolos()
+  }, [cargaSimbolos])
+
   return (
     <>
       <Header>
         <h1>🖼️ Search images 📸 </h1>
       </Header>
       <Content>
-   
+        {photos.map(photo => <Picture key={photo.id} photo={photo} />)}
       </Content>
       <Footer>
         <p>developer: Luisana Henmary Perez Cadenas</p>
---
+++ src/components/Picture.js
@@ -0,0 +1,35 @@
+import styled from "styled-components"
+
+const Article = styled.article`
+    display: inline-block;
+    width: 370px;
+    transition: box-shadow 0.2s ease;
+    margin: 40px 10px 0 10px;
+    background-color: #fff;
+    border-radius: 5px;
+    cursor: pointer;
+
+    & :hover{
+        box-shadow: 0px 3px 5px rgba(0, 0, 0,0.2);
+    }
+
+    & > img{
+        width: inherit;
+        border-radius: 5px 5px 0 0;
+    }
+
+    & > p{
+        margin: 10px 15px;
+    }
+`
+
+const Picture = ({ photo }) => {
+    return (
+        <Article>
+            <img src={photo.urls.regular} alt={photo.alt_description} />
+            <p>{[photo.description, photo.alt_description].join(' - ')} </p>
+        </Article>
+    )
+}
+
+export default Picture
