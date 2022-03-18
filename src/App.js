0e004c6ac759582c9413d67160fc5a146326c4cb
Luisana Perez <luisanahenmaryp@gmail.com>
2022-03-18 12:39:23 -0400

    create and add Footer component

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import Header from "./components/Header";
+import Footer from "./components/Footer";
 
 const App = () => {
   return (
@@ -6,6 +7,11 @@ const App = () => {
       <Header>
         <h1>🖼️ Search images 📸 </h1>
       </Header>
+      <Footer>
+        <p>developer: Luisana Henmary Perez Cadenas</p>
+        <p>Pictures from unsplash.com</p>
+        <p>deploy march 2022</p>
+      </Footer>
     </>
   );
 }
---
+++ src/components/Footer.js
@@ -0,0 +1,16 @@
+import styled from "styled-components"
+
+const Footer = styled.footer`
+    color: rgba(0,0,0,0.4);
+    display: flex;
+    flex-direction: column;
+    justify-content:space-between;
+    align-items: center;
+    padding: 10px; 
+
+    & p{
+        margin: 5px;
+    }
+`
+
+export default Footer
